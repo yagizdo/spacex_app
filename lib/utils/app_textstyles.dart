4207cba50630ac9e5e706d0100391ee677fd149a
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-11-26 14:35:43 +0300

    added sliver app bar

--- lib/utils/app_textstyles.dart
+++ lib/utils/app_textstyles.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:spacex_app/utils/app_colors.dart';
 
 class AppTextStyle {
   AppTextStyle._();
@@ -25,6 +26,16 @@ class AppTextStyle {
   static TextStyle homeText() {
     return TextStyle(
       color: Colors.white,
+      fontSize: 22.sp,
+      fontWeight: FontWeight.w700,
+      letterSpacing: -0.2.sp,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+
+  static TextStyle homeAppbarTitle() {
+    return TextStyle(
+      color: AppColors.blackBackground,
       fontSize: 22.sp,
       fontWeight: FontWeight.w700,
       letterSpacing: -0.2.sp,
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:spacex_app/bloc/launches_bloc.dart';
-import 'package:spacex_app/utils/app_colors.dart';
-import 'package:spacex_app/utils/app_textstyles.dart';
 import 'package:spacex_app/widgets/main_widgets/main_layout.dart';
+
+import '../utils/app_textstyles.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -23,36 +23,57 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return MainLayout(
-      content: Center(
-        child: BlocBuilder<LaunchesBloc, LaunchesState>(
-          builder: (context, state) {
-            if (state is LoadingState) {
-              return Center(
-                child: CircularProgressIndicator(
-                  color: Colors.white,
-                  strokeWidth: 5.w,
-                ),
-              );
-            }
-            if (state is GetLaunchesState) {
-              return ListView.builder(
-                itemCount: state.launches.length,
-                itemBuilder: (context, index) {
-                  var launch = state.launches[index];
+      content: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxScrolled) {
+          return [
+            SliverAppBar(
+              title: Text(
+                'SpaceX Launches',
+                style: AppTextStyle.homeAppbarTitle(),
+              ),
+              backgroundColor: Colors.white,
+              elevation: 0,
+              centerTitle: true,
+              expandedHeight: 200.w,
+              pinned: true,
+            ),
+          ];
+        },
+        body: _buildLaunchesList(),
+      ),
+    );
+  }
 
-                  return Text(
-                    launch.name ?? 'No name data',
-                    style: AppTextStyle.homeText(),
-                  );
-                },
-              );
-            }
-            return Text(
-              'No Launches Data',
-              style: AppTextStyle.homeText(),
+  Widget _buildLaunchesList() {
+    return Center(
+      child: BlocBuilder<LaunchesBloc, LaunchesState>(
+        builder: (context, state) {
+          if (state is LoadingState) {
+            return Center(
+              child: CircularProgressIndicator(
+                color: Colors.white,
+                strokeWidth: 5.w,
+              ),
             );
-          },
-        ),
+          }
+          if (state is GetLaunchesState) {
+            return ListView.builder(
+              itemCount: state.launches.length,
+              itemBuilder: (context, index) {
+                var launch = state.launches[index];
+
+                return Text(
+                  launch.name ?? 'No name data',
+                  style: AppTextStyle.homeText(),
+                );
+              },
+            );
+          }
+          return Text(
+            'No Launches Data',
+            style: AppTextStyle.homeText(),
+          );
+        },
       ),
     );
   }
