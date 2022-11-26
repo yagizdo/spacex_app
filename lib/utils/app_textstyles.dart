1ea4ceb3b64bb7a933ad46d50337ad0b05c0a856
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-11-26 15:56:21 +0300

    remove unused custom textstyle

--- lib/utils/app_textstyles.dart
+++ lib/utils/app_textstyles.dart
@@ -5,24 +5,6 @@ import 'package:spacex_app/utils/app_colors.dart';
 class AppTextStyle {
   AppTextStyle._();
 
-  // Example
-  /*static TextStyle titleMedium({
-    Color? color,
-    double? size,
-    FontWeight? weight,
-    TextOverflow? overflow,
-    bool isItalic = false,
-  }) {
-    return TextStyle(
-      color: color ?? Colors.white,
-      fontSize: (size ?? 22).sp,
-      fontWeight: weight ?? FontWeight.w700,
-      letterSpacing: -0.2.sp,
-      overflow: overflow ?? TextOverflow.ellipsis,
-      //fontStyle: isItalic ? FontStyle.italic : FontStyle.normal
-    );
-  }*/
-
   static TextStyle homeText() {
     return TextStyle(
       color: Colors.white,
